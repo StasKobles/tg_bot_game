d81b9c03bec3bbd020668182b3622525dabf76ac
Stas Kobles <sskobles@gmail.com>
2022-11-16 17:18:52 +0400

    Refactoring

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const TelegramApi = require("node-telegram-bot-api");
+const { gameOptions, againOptions } = require("./gameOptions");
 
 require("dotenv").config();
 const token = process.env.token;
@@ -7,34 +8,6 @@ const bot = new TelegramApi(token, { polling: true });
 
 const chats = [];
 
-const gameOptions = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [
-      [
-        { text: "1", callback_data: "1" },
-        { text: "2", callback_data: "2" },
-        { text: "3", callback_data: "3" },
-      ],
-      [
-        { text: "4", callback_data: "4" },
-        { text: "5", callback_data: "5" },
-        { text: "6", callback_data: "6" },
-      ],
-      [
-        { text: "7", callback_data: "7" },
-        { text: "8", callback_data: "8" },
-        { text: "9", callback_data: "9" },
-      ],
-      [{ text: "0", callback_data: "0" }],
-    ],
-  }),
-};
-
-const againOptions = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [[{ text: "Play again", callback_data: "/again" }]],
-  }),
-};
 const startGame = async (chatId) => {
   await bot.sendMessage(chatId, "I will guess a number from 1 to 9");
   const number = Math.floor(Math.random() * 10);
